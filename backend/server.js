1c4b8bddd0a6c8cba6eb41100e6b3ee2262c8d37
Ayush Kumar <ayushk2.ug19.cs@nitp.ac.in>
2024-02-14 16:00:04 +0530

    again

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+    console.log(__dirname);
 const app=express();
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
 
 app.get('/',(req,res)=>
 {
-    res.sendFile(path.resolve(__dirname,"/public/index.html"));
+    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"));
 })
 app.listen(3000,()=>
 {
